d2c0c47217a7a6c80ba09dbe1ad25126c966376f
Nikita Strigunkov <Nikita Strigunkov>
2017-04-08 18:56:10 +0300

    Initial commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,105 @@
+
+function scroll_to(clicked_link, nav_height) {
+	var element_class = clicked_link.attr('href').replace('#', '.');
+	var scroll_to = 0;
+	if(element_class != '.top-content') {
+		element_class += '-container';
+		scroll_to = $(element_class).offset().top - nav_height;
+	}
+	if($(window).scrollTop() != scroll_to) {
+		$('html, body').stop().animate({scrollTop: scroll_to}, 1000);
+	}
+}
+
+
+jQuery(document).ready(function() {
+	
+	/*
+	    Navigation
+	*/
+	$('a.scroll-link').on('click', function(e) {
+		e.preventDefault();
+		scroll_to($(this), $('nav').outerHeight());
+	});
+	// toggle "navbar-no-bg" class
+	$('.top-content .text').waypoint(function() {
+		$('nav').toggleClass('navbar-no-bg');
+	});
+	
+    /*
+        Background slideshow
+    */
+    //$('.how-it-works-container').backstretch("assets/img/backgrounds/1.jpg");
+    $('.call-to-action-container').backstretch("assets/img/backgrounds/1.jpg");
+    
+    $('#top-navbar-1').on('shown.bs.collapse', function(){
+    	//$('.top-content').backstretch("resize");
+    });
+    $('#top-navbar-1').on('hidden.bs.collapse', function(){
+    	//$('.top-content').backstretch("resize");
+    });
+    
+    /*
+        Wow
+    */
+    new WOW().init();
+	
+});
+
+
+jQuery(window).load(function() {
+	
+	/*
+		Loader
+	*/
+	$(".loader-img").fadeOut();
+	$(".loader").delay(1000).fadeOut("slow");
+	
+	/*
+		Hidden images
+	*/
+	$(".testimonial-image img").attr("style", "width: auto !important; height: auto !important;");
+	
+});
+
+function getTimeRemaining(endtime) {
+    var t = Date.parse(endtime) - Date.parse(new Date());
+    var seconds = Math.floor((t / 1000) % 60);
+    var minutes = Math.floor((t / 1000 / 60) % 60);
+    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+    var days = Math.floor(t / (1000 * 60 * 60 * 24));
+    return {
+        'total': t,
+        'days': days,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds
+    };
+}
+
+function initializeClock(id, endtime) {
+    var clock = document.getElementById(id);
+    var daysSpan = clock.querySelector('.days');
+    var hoursSpan = clock.querySelector('.hours');
+    var minutesSpan = clock.querySelector('.minutes');
+    var secondsSpan = clock.querySelector('.seconds');
+
+    function updateClock() {
+        var t = getTimeRemaining(endtime);
+
+        daysSpan.innerHTML = t.days;
+        hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+        minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+        secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+        if (t.total <= 0) {
+            clearInterval(timeinterval);
+        }
+    }
+
+    updateClock();
+    var timeinterval = setInterval(updateClock, 1000);
+}
+
+var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
+initializeClock('clockdiv', deadline);
